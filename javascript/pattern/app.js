63a39e1e32f4bbae0ac425f1d6145b2e80ffac0e
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-08-10 23:06:11 +0530

    added number pattern

---
+++ javascript/pattern/app.js
@@ -0,0 +1,226 @@
+// function pyramid(size) {
+//   for (let i = 0; i <= size; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i; j++) {
+//       row += "*";
+//     }
+//     console.log(row);
+//   }
+// }
+// pyramid(5);
+
+// function square(size) {
+//   for (let i = 1; i <= size; i++) {
+//     row = "";
+//     for (let j = 1; j <= size; j++) {
+//       row += "*";
+//     }
+//     console.log(row);
+//   }
+// }
+// square(5);
+
+// function pyramidInverted(size) {
+//   for (let i = size; i >= 1; i--) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//       row += "*";
+//     }
+//     console.log(row);
+//   }
+// }
+// pyramidInverted(5);
+
+// function pyramidInverted1(size) {
+//   for (let i = 1; i <= size; i++) {
+//     let row = "";
+//     let spaces = "";
+
+//     for (let j = 0; j < size - i; j++) {
+//       spaces += " ";
+//     }
+//     for (let k = 0; k < 2 * i - 1; k++) {
+//       row += "*";
+//     }
+//     console.log(spaces + row);
+//   }
+// }
+// pyramidInverted1(4);
+
+// function add(n) {
+//   for (let i = 0; i <= n; i++) {
+//     let str = "";
+//     for (let j = 1; j <= n - i; j++) {
+//       str += "*";
+//     }
+//     console.log(str);
+//   }
+// }
+// add(5);
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// function add1(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//     let spaces = "";
+//     for (let j = 1; j <= i; j++) {
+//       spaces += " ";
+//     }
+//     for (let k = 0; k <= 2 * i - 1; k++) {
+//       str += "*";
+//     }
+//     console.log(spaces + str + spaces);
+//   }
+// }
+// add1(5);
+
+// *********
+// *******
+//  *****
+//   ***
+//    *
+
+// function add(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//     let spaces = "";
+//     for (let j = 0; j <= n - i; j++) {
+//       spaces += " ";
+//     }
+//     for (let k = 0; k < 2 * i - 1; k++) {
+//       str += "*";
+//     }
+
+//     console.log(spaces + str + spaces);
+//   }
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//     let spaces = "";
+//     for (let j = 1; j <= i; j++) {
+//       spaces += " ";
+//     }
+//     for (let k = 0; k <= 2 * (n - i); k++) {
+//       str += "*";
+//     }
+//     console.log(spaces + str + spaces);
+//   }
+// }
+// add(4);
+
+//     *
+//    ***
+//   *****
+//  *******
+//  *******
+//   *****
+//    ***
+//     *
+
+// function hallowSquqre(n) {
+//   for (let i = 0; i <= n - 1; i++) {
+//     let str = "";
+
+//     for (let j = 0; j <= n - 1; j++) {
+//       if (i === 0 || i === n - 1) {
+//         str += "*";
+//       } else {
+//         if (j === 0 || j === n - 1) {
+//           str += "*";
+//         } else {
+//           str += " ";
+//         }
+//       }
+//     }
+
+//     console.log(str);
+//   }
+// }
+
+// hallowSquqre(5);
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+// function add(n) {
+//   for (let i = 1; i <= n; i++) {
+//     var spaces = "";
+//     var str = "";
+//     for (var j = 1; j <= n - i; j++) {
+//       spaces += " ";
+//     }
+//     for (let k = 1; k <= 2 * i - 1; k++) {
+//       if (i == 1 || i == n) {
+//         str += "*";
+//       } else {
+//         if (k == 1 || k == 2 * i - 1) {
+//           str += "*";
+//         } else {
+//           str += " ";
+//         }
+//       }
+//     }
+//     console.log(spaces + str);
+//   }
+// }
+// add(5);
+
+//     *
+//    * *
+//   *   *
+//  *     *
+// *********
+
+// function addd(n) {
+//   for (let i = n; i >= 1; i--) {
+//     let spaces = "";
+//     let str = "";
+
+//     for (let j = 1; j <= n - i; j++) {
+//       spaces += " ";
+//     }
+
+//     for (let k = 1; k <= 2 * i - 1; k++) {
+//       if (i == 1 || i == n) {
+//         str += "*";
+//       } else {
+//         if (k == 1 || k == 2 * i - 1) {
+//           str += "*";
+//         } else {
+//           str += " ";
+//         }
+//       }
+//     }
+
+//     console.log(spaces + str + spaces);
+//   }
+// }
+
+// addd(5);
+
+// *********
+//  *     *
+//   *   *
+//    * *
+//     *
+
+for (let i = 1; i <= 4; i++) {
+  let line = "";
+  for (let j = 1; j <= 3; j++) {
+    if (i === 1 || i === 4 || j === 1 || j === 3) {
+      line += "3";
+    } else if (i === 2 && j === 2) {
+      line += "1";
+    } else if (i === 3 && j === 2) {
+      line += "2";
+    }
+  }
+  console.log(line);
+}
