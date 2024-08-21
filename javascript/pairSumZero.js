1a76bc603433aac992377a75d7f069ac49b5e8c9
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-08-21 11:33:27 +0530

    optimize method of pair sum zero

--- javascript/pairSumZero.js
+++ javascript/pairSumZero.js
@@ -1,3 +1,5 @@
+//calculate the first index pair which sum is zero then return indexes
+
 function sumZero() {
   const arr = [1, -1, 2, 8, 5];
   for (let i = 0; i < arr.length; i++) {
@@ -12,3 +14,25 @@ function sumZero() {
 }
 
 console.log(sumZero());
+
+//second approach
+
+function sumZero1() {
+  const arr = [1, -1, 2, 8, 5];
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+
+    if (sum === 0) {
+      return [left, right];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+console.log(sumZero1());
