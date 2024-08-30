b09e17d0432c11cb49bbc88a7586c8c1bca389fa
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-08-31 02:29:25 +0530

    reverse string

--- javascript/array/reverse.js
+++ javascript/array/reverse.js
@@ -1,5 +1,6 @@
-function reverseArray() {
-  let arr = [1, 4, 2, 55, 88, 77];
+let arr = [1, 4, 2, 55, 88, 77];
+
+function reverseArray(arr1) {
   let a = 0;
   let b = arr.length - 1;
 
@@ -13,4 +14,24 @@ function reverseArray() {
   return arr;
 }
 
-console.log(reverseArray());
+console.log(reverseArray(arr));
+
+function reverseString(str) {
+  let arr = str.split("");
+  console.log(arr);
+  let i = 0;
+  let j = arr.length - 1;
+
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+
+    i++;
+    j--;
+  }
+
+  return arr.join("");
+}
+
+console.log(reverseString("hello"));
