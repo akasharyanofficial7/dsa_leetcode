0325514654ab773102f42cc2b123fcd099a25bc3
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-09-01 03:54:56 +0530

    find Rthird greater element

--- javascript/array/maxSum.js
+++ javascript/array/maxSum.js
@@ -18,3 +18,31 @@ function sum(arr) {
 }
 let arr = [1, 98, 10, 3, 60, 22, 10];
 console.log(sum(arr));
+
+// find the third greater element
+
+//find the max sum  of the element of the given array
+function ThirdGreaterElement(arr1) {
+  let max = -Infinity;
+  let sMax = -Infinity;
+  let tMax = -Infinity;
+  let i = 0;
+  let j = arr1.length - 1;
+  while (i <= j) {
+    if (arr1[i] > max) {
+      tMax = sMax;
+      sMax = max;
+      max = arr[i];
+    } else if (arr1[i] > sMax && arr1[i] < max) {
+      tMax = sMax;
+      sMax = arr1[i];
+    } else if (arr1[i] > sMax && arr1[i] < sMax) {
+      tMax = arr1[i];
+    }
+
+    i++;
+  }
+  return tMax;
+}
+let arr1 = [1, 8, 10, 3, 60, 22, 100];
+console.log(ThirdGreaterElement(arr1));
