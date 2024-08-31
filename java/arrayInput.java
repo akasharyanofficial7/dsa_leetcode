c33c55de9c05954021936ec1d589d98b57aac615
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-08-31 19:53:31 +0530

    sum of array

--- java/arrayInput.java
+++ java/arrayInput.java
@@ -1,22 +1,47 @@
 //taking input array by compiler 
 import java.util.Scanner;
+// public class arrayInput{
+//     public  static void main(String [] args){
+// Scanner sc= new Scanner(System.in);
+// int n= sc.nextInt();
+// int[] arr= new int[n];
+// for( int i=0; i<n; i++){
+//      arr[i]= sc.nextInt();
+// }
+//  for( int i=0; i<n; i++){
+
+// System.out.print(arr[i] +" ");
+
+//  }
+//     }
+// }
+
+
+
+
+
+//sum of all element
 public class arrayInput{
-    public  static void main(String [] args){
+public static void main(String [] args){
 Scanner sc= new Scanner(System.in);
 int n= sc.nextInt();
-int[] arr= new int[n];
+int[] arr= new int[10];
 for( int i=0; i<n; i++){
-     arr[i]= sc.nextInt();
+arr[i]= sc.nextInt();
 }
- for( int i=0; i<n; i++){
 
-System.out.print(arr[i] +" ");
+
+
+int sum=0;
+ for( int i=0; i<n; i++){
+    sum= n*(n+1)/2;
 
  }
-    }
+
+ System.out.println(sum);
 }
 
-
+}
 
 
  
