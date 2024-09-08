3e99db5e1a01ad2ce4a39bd1cef0773fd3542324
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-09-09 01:01:46 +0530

    create basic crud

---
+++ task3/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const value = [];
+  const [data, setData] = useState(value);
+
+  const [val, setVal] = useState("");
+  const deleteList = (index) => {
+    const copyArray = [...data];
+
+    copyArray.splice(index, 1);
+    setData(copyArray);
+    console.log(copyArray);
+  };
+
+  const createList = () => {
+    setData([val, ...data]);
+  };
+  return (
+    <div>
+      <ul>
+        {data.map((val, index) => (
+          <li>
+            {val}
+            <button onClick={() => deleteList(index)}>delete </button>
+          </li>
+        ))}
+      </ul>
+
+      <input onChange={(e) => setVal(e.target.value)} />
+
+      <button onClick={createList}>Add</button>
+
+      <h2>{val}</h2>
+    </div>
+  );
+};
+
+export default App;
